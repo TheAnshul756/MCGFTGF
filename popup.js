04a7af31ad5b55a803772c1a2bb93fdd14e1a090
Eerie16 <amritansh.singhal.cse17@itbhu.ac.in>
2021-06-04 01:07:21 +0530

    Add favicons and permissions

--- popup.js
+++ popup.js
@@ -4,4 +4,5 @@ btn.onclick = function () {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "get-modal"});
     });
+    window.close();
 }
